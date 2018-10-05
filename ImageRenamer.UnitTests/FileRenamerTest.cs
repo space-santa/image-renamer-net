ba3e212052ee0089df693fa604ce2d2ee8ce2681
claus <claus.z@protonmail.com>
2018-10-05 10:27:37 +1000

    Add FileRenamer
    This makes TimestampGenerator redundant. For now.

---
+++ ImageRenamer.UnitTests/FileRenamerTest.cs
@@ -0,0 +1,46 @@
+using System;
+using Xunit;
+using ImageRenamer;
+using System.IO;
+
+namespace ImageRenamer.UnitTests
+{
+    public class FileRenamerTest
+    {
+        private readonly string expected = "2018-06-13_15.23.30";
+
+        [Fact]
+        public void TestEmptyPathShouldThrow()
+        {
+            Exception ex = Assert.Throws<ArgumentException>(() => FileRenamer.GetNewName(""));
+            Assert.Equal("Can't process empty string.", ex.Message);
+        }
+
+        [Fact]
+        public void TestFileNotFoundShouldThrow()
+        {
+            Assert.Throws<FileNotFoundException>(() => FileRenamer.GetNewName("idontexist"));
+        }
+
+        [Fact]
+        public void TestBadStringShouldThrow()
+        {
+            Exception ex = Assert.Throws<TimestampException>(() => FileRenamer.GetNewName("blarg"));
+            Assert.Equal("String blarg doesn't match any format.", ex.Message);
+        }
+
+        [Fact]
+        public void TestImgStringShouldReturnExpected()
+        {
+            string result = FileRenamer.GetNewName("IMG_20180613_152330.jpg");
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void TestVidStringShouldReturnExpected()
+        {
+            string result = FileRenamer.GetNewName("VID_20180613_152330.mp4");
+            Assert.Equal(expected, result);
+        }
+    }
+}
---
+++ ImageRenamer/FileRenamer.cs
@@ -0,0 +1,97 @@
+using ExifLib;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+
+namespace ImageRenamer
+{
+    public class MissingTagException : Exception
+    {
+        public MissingTagException()
+        : base("File has no DateTimeOriginal tag.")
+        {
+        }
+    }
+
+    public class TimestampException : Exception
+    {
+        public TimestampException(string message)
+        : base(message)
+        {
+        }
+    }
+
+    public static class FileRenamer
+    {
+        public static string GetNewName(string path)
+        {
+            DateTime origTimestamp;
+
+            try
+            {
+                origTimestamp = GetOrigDateTime(path);
+            }
+            catch (Exception ex)
+            {
+                if (ex is MissingTagException || ex is ArgumentException || ex is System.IO.EndOfStreamException)
+                {
+                    origTimestamp = GetDateTimeFromFilename(path);
+                }
+                else
+                {
+                    throw ex;
+                }
+            }
+
+            return FormatDateTime(origTimestamp);
+        }
+        private static string FormatDateTime(DateTime dateTime)
+        {
+            return dateTime.ToString("yyyy-MM-dd_HH.mm.ss");
+        }
+
+        private static DateTime GetDateTimeFromFilename(string fileName)
+        {
+            if (fileName.Length == 0)
+            {
+                throw new ArgumentException("Can't process empty string.");
+            }
+
+            List<string> formats = new List<string>();
+            formats.Add("yyyy\\:MM\\:dd HH\\:mm\\:ss");
+            formats.Add("I\\MG_yyyyMMdd_HHmmss.jp\\g");
+            formats.Add("VID_yyyyMMdd_HHmmss.\\mp4");
+
+            foreach (string format in formats)
+            {
+                try
+                {
+                    DateTime dt = DateTime.ParseExact(fileName, format, CultureInfo.InvariantCulture);
+                    return dt;
+                }
+                catch (FormatException)
+                {
+                    Debug.WriteLine($"{fileName} doesn't match {format}");
+                }
+            }
+
+            throw new TimestampException($"String {fileName} doesn't match any format.");
+        }
+
+        private static DateTime GetOrigDateTime(string path)
+        {
+            using (ExifReader reader = new ExifReader(path))
+            {
+                // Extract the tag data using the ExifTags enumeration
+                DateTime datePictureTaken;
+                if (reader.GetTagValue<DateTime>(ExifTags.DateTimeOriginal, out datePictureTaken))
+                {
+                    return datePictureTaken;
+                }
+
+                throw new MissingTagException();
+            }
+        }
+    }
+}
