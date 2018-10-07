99afd74663525ed187c3431d83bdfc3b542e6b09
claus <claus.z@protonmail.com>
2018-10-07 22:40:25 +1100

    Add a suffix to the name if it already exists

--- ImageRenamer/Mover.cs
+++ ImageRenamer/Mover.cs
@@ -1,3 +1,6 @@
+using System;
+using System.IO;
+
 namespace ImageRenamer
 {
     public interface IMover
@@ -7,8 +10,28 @@ namespace ImageRenamer
 
     public class Mover : IMover
     {
+        private static string ChangeNameIfExists(string path)
+        {
+            string oldName = Path.GetFileNameWithoutExtension(path);
+            string newName = path;
+            string extension = Path.GetExtension(path).ToLower();
+            string folder = Directory.GetParent(path).ToString();
+
+            int i = 1;
+            while (File.Exists(newName))
+            {
+                newName = Path.Combine(folder, $"{oldName}_({i}){extension}");
+                i += 1;
+            }
+
+            return newName;
+        }
+
         public void Move(string from, string to)
         {
+            to = ChangeNameIfExists(to);
+            Console.WriteLine($"Rename {from} -> {to}");
+            
             System.IO.File.Move(from, to);
         }
     }
