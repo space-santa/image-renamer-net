628635ee5f7fdcf26c9b3254bd872c040de9ff9c
space-santa <claus.z@protonmail.com>
2019-04-20 21:58:32 +1000

    Fully automate DependencyGroup generation

--- wix/create-dependencygroup.ps1
+++ wix/create-dependencygroup.ps1
@@ -1,20 +1,5 @@
-heat.exe dir ..\ImageRenamer.Wpf\bin\Release\netcoreapp3.0\publish -cg DependencyGroup -out DependencyGroup.wxs
+heat.exe dir ..\ImageRenamer.Wpf\bin\Release\netcoreapp3.0\publish -cg DependencyGroup -dr INSTALLDIR -gg -g1 -out DependencyGroup.wxs
 
-$XMLfile = '.\DependencyGroup.wxs'
-[XML]$xml = Get-Content $XMLfile
-$target = $xml.Wix.Fragment.DirectoryRef | Where-Object { $_.id -eq "TARGETDIR" }
-$targetId = $target.directory.id
-
-$cleanGuids = (Get-Content .\DependencyGroup.wxs) | ForEach-Object {
-    $_ -replace 'PUT-GUID-HERE', [guid]::NewGuid()
-}
-
-$cleanDirs = $cleanGuids | ForEach-Object {
-    $_ -replace $targetId, "INSTALLDIR"
-}
-
-$cleanDirs | ForEach-Object {
+(Get-Content .\DependencyGroup.wxs) | ForEach-Object {
     $_ -replace 'SourceDir', "..\ImageRenamer.Wpf\bin\Release\netcoreapp3.0\publish"
 } | Set-Content .\DependencyGroup.wxs
-
-# Now remove the targetdir fragment.
